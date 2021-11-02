a0e527a9a5a80f4e628a3855bef27742f3ed9694
何义乾 <16231306@bjtu.edu.cn>
2021-11-02 18:34:13 +0800

    增加 AI studio-链接

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 # First stage
 
-nohup python -u main.py --window_size 100 --batch_size 32 --stage 1 --num_queries 32 --point_prob_normalize > log/train_step1.log 2>&1 &
+python -m main.py --window_size 100 --batch_size 32 --stage 1 --num_queries 32 --point_prob_normalize
 
 # Second stage for relaxation mechanism
 
-nohup python -u main.py --window_size 100 --batch_size 32 --lr 1e-5 --stage 2 --epochs 10 --lr_drop 5 --num_queries 32 --point_prob_normalize --load outputs/checkpoint_best_sum_ar.pth > log/train_step2.log 2>&1 &
+python -m main.py --window_size 100 --batch_size 32 --lr 1e-5 --stage 2 --epochs 10 --lr_drop 5 --num_queries 32 --point_prob_normalize --load outputs/checkpoint_best_sum_ar.pth
 
 # Third stage for completeness head
 
-nohup python -u main.py --window_size 100 --batch_size 32 --lr 1e-4 --stage 3 --epochs 20 --num_queries 32 --point_prob_normalize --load outputs/checkpoint_best_sum_ar.pth > log/train_step3.log 2>&1 &
+python -m main --window_size 100 --batch_size 32 --lr 1e-4 --stage 3 --epochs 20 --num_queries 32 --point_prob_normalize --load outputs/checkpoint_best_sum_ar.pth
